7de81ebd3a475931e549b77b11ecdd03be2c40ec
lewap09052 <pawelniemiec1@msn.com>
2022-09-20 19:38:31 +0100

    added 4 loops

---
+++ src/Switch.java
@@ -0,0 +1,13 @@
+public class Switch {
+    public static void main(String[] args) {
+        int x = 2;
+        switch (x){
+            case 1:
+                System.out.println("Pawel is driving");
+                break;
+            case 2:
+                System.out.println("Pawel stopped car");
+                break;
+        }
+    }
+}
