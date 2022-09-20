89b560a94647f816e6d5894995c3659e1a11771e
lewap09052 <pawelniemiec1@msn.com>
2022-09-20 19:12:49 +0100

    added 4 loops

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,13 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+
+        int x=0;
+        do{
+            x++;
+            System.out.println(x);
+
+
+        }while(x<10);
+    }
+}
