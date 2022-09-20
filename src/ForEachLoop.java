fdce6ddb6b8bbd34d54b75bbebfbf1d826b7bd55
lewap09052 <pawelniemiec1@msn.com>
2022-09-20 19:02:47 +0100

    added 4 loops

---
+++ src/ForEachLoop.java
@@ -0,0 +1,9 @@
+public class ForEachLoop {
+    public static  void main(String[] args) {
+      //ForEachLoop
+        String[] cars=new String[]{"Audi", "BMW", "Ford", "Mercedes" };
+        for(String car:cars){
+            System.out.println(car);
+        }
+    }
+}
